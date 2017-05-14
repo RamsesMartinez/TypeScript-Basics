9cbfea49b26e5ec1cfe96845c949c76d538e252c
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-05-14 03:19:49 -0500

    Añadido ejemplo de uso de let y var

--- prueba.js
+++ prueba.js
@@ -5,4 +5,20 @@ var nombre = "Ramsés Martínez";
 var resultado = holaMundo(nombre);
 var etiqueta = document.getElementById('container-saludo');
 etiqueta.innerHTML = holaMundo(nombre);
+// Variables y Tipos
+var nombre = "Ramses Martinez";
+var edad = 20; // :'c
+var programador = true;
+var langs = ['Java', 'Javascipt', 'Python'];
+etiqueta.innerHTML = nombre + ' - ' + edad;
 console.log('Excelente!');
+// Diferencia entre let y var
+var a = 10;
+var b = 2;
+console.log('Valores iniciales: \t' + a + ' - ' + b);
+if (a === 10) {
+    var a_1 = 5;
+    var b = 25;
+    console.log('Valores dentro del if: \t' + a_1 + ' - ' + b);
+}
+console.log('Valores fuera del if: \t' + a + ' - ' + b);
--- prueba.ts
+++ prueba.ts
@@ -8,4 +8,28 @@ var resultado = holaMundo(nombre);
 var etiqueta = <HTMLElement>document.getElementById('container-saludo');
 etiqueta.innerHTML = holaMundo(nombre);
 
+// Variables y Tipos
+var nombre:string = "Ramses Martinez";
+var edad:number = 20; // :'c
+var programador:boolean = true;
+var langs:Array<string> = ['Java', 'Javascipt', 'Python']
+
+etiqueta.innerHTML = nombre + ' - ' + edad;
+
 console.log('Excelente!');
+
+
+// Diferencia entre let y var
+
+var a = 10;
+var b = 2;
+console.log('Valores iniciales: \t' + a + ' - ' + b);
+
+if(a === 10) {
+	let a = 5;
+	var b = 25;
+	console.log('Valores dentro del if: \t' + a + ' - ' + b);
+}
+
+	console.log('Valores fuera del if: \t' + a + ' - ' + b);
+
